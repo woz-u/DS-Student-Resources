3585e57f9cdacb0261886081117555baf98d9f00
Joseph Raetano <joseph.raetano@woz-u.com>
2021-10-07 19:25:25 -0400

    deleted old files

--- DS101-Basic-Statistics/Installation/a.r
+++
@@ -1,5 +0,0 @@
-install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest'))
-
-devtools::install_github('IRkernel/IRkernel')
-
-IRkernel::installspec(user = FALSE)
