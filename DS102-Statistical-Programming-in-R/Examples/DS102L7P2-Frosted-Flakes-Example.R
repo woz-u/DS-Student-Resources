1e7df2f023841b18acc8ea85e6cca6cb88b01074
Joseph Raetano <joseph.raetano@woz-u.com>
2021-12-02 21:10:16 -0500

    updates

---
+++ DS102-Statistical-Programming-in-R/Examples/DS102L7P2-Frosted-Flakes-Example.R
@@ -0,0 +1,21 @@
+# DS102L7P2 Frosted Flakes Example
+
+install.packages("ggplot2")
+library(ggplot2)
+
+frostedflakes = read.csv('../Data/frostedflakes.csv')
+
+head(frostedflakes)
+
+t_obj <- t.test(frostedflakes$Lab, mu = 37)
+
+print(t_obj)
+
+d <- ggplot(frostedflakes, aes(x = Lab))
+d + geom_histogram(binwidth = 1) +
+geom_vline(xintercept = t_obj$conf.int[1], color = "red") +
+geom_vline(xintercept = t_obj$conf.int[2], color = "red") +
+geom_vline(xintercept = t_obj$null.value, color = "green")
+
+ggplot(frostedflakes, aes(sample = Lab)) + geom_qq()
+
