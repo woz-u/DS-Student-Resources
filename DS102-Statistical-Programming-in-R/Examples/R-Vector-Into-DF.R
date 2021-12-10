337873229cd1975d37a2be0e841b88ede3b6d049
Matthew Gerardino <matthewgerardino@gmail.com>
2021-12-10 16:09:53 -0600

    Update R-Vector-Into-DF.R

--- DS102-Statistical-Programming-in-R/Examples/R-Vector-Into-DF.R
+++ DS102-Statistical-Programming-in-R/Examples/R-Vector-Into-DF.R
@@ -1,7 +1,16 @@
-# Turn any vector into a column in R
-
+# Turn any vector into a column in R Example 1
 df = matrix(LakeHuron)
 
 colnames(df)[1] <- c('My_Column')
 View(df)
 
+
+
+# Turn any vector into a Data Frame in R
+head.matrix(nhtemp)
+
+first25 <- nhtemp[1:25]
+last25 <- nhtemp[36:60]
+
+data <- data.frame(first25, last25)
+View(data)
