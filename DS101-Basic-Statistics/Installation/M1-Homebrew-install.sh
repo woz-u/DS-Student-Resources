9662e04f4e8d481da0e88009841bd354c7a76bc8
Joseph Raetano <joseph.raetano@woz-u.com>
2022-01-03 21:44:18 -0500

    cleaned up homebrew scripts

--- DS101-Basic-Statistics/Installation/M1-Homebrew-install.sh
+++ DS101-Basic-Statistics/Installation/M1-Homebrew-install.sh
@@ -124,21 +124,6 @@ brew install node
 #sudo change owner for Jupyter lab to run
 sudo chown -R $(whoami) /Users/$USER/Library/Jupyter
 
-# Install R kernelspec
-# change directory to the R folder
-# echo "changing directory to R folder"
-# cd /Library/Frameworks/R.framework/Versions/4.1
-# echo "ensure you're in the right directory"
-# pwd
-
-# curl a.r
-#echo "curl a.r from github"
-#curl -O https://github.com/Wildertrek/Jupyter-Latex/blob/master/install_scripts/a.r
-
-# Start R terminal
-#echo "R Terminal start and run script"
-#Rscript a.r
-
 #run brew list
 brew list
 
--- DS101-Basic-Statistics/Installation/i86-Homebrew-install.sh
+++ DS101-Basic-Statistics/Installation/i86-Homebrew-install.sh
@@ -107,22 +107,6 @@ brew install node
 brew list
 brew services list
 
-# Install R kernelspec
-# change directory to the R folder
-# echo "changing directory to R folder"
-# cd /Library/Frameworks/R.framework/Versions/4.1
-# echo "ensure you're in the right directory"
-# pwd
-
-# curl a.r
-#echo "curl a.r from github"
-#curl -O https://github.com/Wildertrek/Jupyter-Latex/blob/master/install_scripts/a.r
-
-# Start R terminal
-#echo "R Terminal start and run script"
-#Rscript a.r
-
-
 echo "*******************************************************************"
 echo "Congratulations! Main Data Science applications have been installed"
 echo "Check the brew list above to ensure all applications are installed"
